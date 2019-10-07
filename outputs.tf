c95960f4f2489b94dfe3d983a240c85fe334767c
Mark Menger <m.menger@f5.com>
2019-10-07 14:04:02 -0700

    outputs

---
+++ outputs.tf
@@ -0,0 +1,24 @@
+output "vpc" {
+  description = "AWS VPC ID for the created VPC"
+  value       = ""
+}
+
+output "bigip_mgmt_public_ips" {
+  description = "Public IP addresses for the BIG-IP management interfaces"
+  value       = ""
+}
+
+output "bigip_mgmt_port" {
+  description = "BIG-IP management port"
+  value       = ""
+}
+
+output "bigip_password" {
+  description = "BIG-IP management password"
+  value       = ""
+}
+
+output "nginx_ips" {
+  description = "Internal IP addresses of the demo app servers"
+  value       = ""
+}
